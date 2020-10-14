f7af198beda1c5a622fd6b6d5be8748ee1834cc4
dimasdevspro <dimasdevspro@github.com>
2020-10-13 22:17:48 -0300

    second commit

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -1,4 +1,13 @@
-const map = L.map('mapid').setView([-27.2205149,-49.6462334], 16);
+const options = {
+    dragging: false,
+    touchZoom: false,
+    doubleClickZoom: false,
+    scrollWhellZoom: false,
+    zoomControl: false
+}
+
+
+const map = L.map('mapid', options).setView([-27.2205149,-49.6462334], 16);
 
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
@@ -10,14 +19,30 @@ const icon = L.icon({
     popupAnchor: [170, 2]
 })
 
-const popup = L.popup({
-    closeButton: false,
-    className: 'map-popup',
-    miniWidth: 240,
-    miniHeigth: 240
-}).setContent('Lar das meninas <a href="orphanage.html?id=1" class=""choose-orphanage"><img src="./public/images/arrow-white.svg"></a>')
 
 L.marker([-27.2205149,-49.6462334], { icon })
 .addTo(map)
-.bindPopup(popup)
+
+
+function selectImage(event) {
+const button = event.currentTarget
+
+console.log(button.children)
+
+//remover todas as classes .active
+const buttons =document.querySelectorAll(".images button")
+buttons.forEach(removeActiveClass)
+
+function removeActiveClass(button){
+    button.classList.remove("active")
+}
+//selecionar a image clicada
+const image = button.children[0]
+const imageContainer = document.querySelector(".orphanage-details > img")
+//atualizar o container de image
+imageContainer.src = image.src
+//adicionar a classe .active para este botao
+    button.classList.add('active')
+
+}
     
